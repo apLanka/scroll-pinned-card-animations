380a2503f570143d25c7565f5c8744438c9e246e
Pasindu Lanka <106298729+apLanka@users.noreply.github.com>
2024-12-08 22:38:19 +0530

    feat: remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,6 @@
-'use client'
-
 import AnimatedCardSection from "@/app/_component/animated-card-section";
-import {useEffect, useState} from "react";
 
 export default function Home() {
-    const [isClient, setIsClient] = useState(false)
-
-    useEffect(() => {
-        setIsClient(true)
-    }, [])
-
     return (
         <div>
             <div className="h-screen bg-black"></div>
